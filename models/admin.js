6387cc9050ff39413eb54b8a60d8f46ed2dc3bfb
Vedant  Nandoskar <vedant.nandoskar@gmail.com>
2018-07-27 22:06:10 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -12,24 +12,5 @@ var adminSchema = new mongoose.Schema({
 adminSchema.plugin(passportLocalMongoose);
 
 
-//One time Admin Register
-//  app.get("/registerWithAWink", function (req, res) {
-//    res.render("register");
-//  });
-//
-// app.post("/registerWithAWink", function (req, res) {
-//   var newAdmin = new admin({username: req.body.username});
-//   admin.register(newAdmin, req.body.password, function (err, admin) {
-//                            if(err){
-//                              console.log("Error in admin: ", err);
-//                              return res.render("register");
-//                            }
-//                            passport.authenticate("local")(req, res, function (){
-//                              res.redirect("/admin");
-//                              console.log("Admin added successfully");
-//                            });
-//                          });
-// });
-
 //Export model
 module.exports = mongoose.model("admin", adminSchema);
